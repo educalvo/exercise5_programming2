636a211288a1f2406b9a10197de5e7b872fa2214
educalvo <educalvo99@gmail.com>
2018-01-10 22:53:01 +0100

    Update exercise5.java

--- exercise5.java
+++ exercise5.java
@@ -14,29 +14,40 @@ public class Exercise5
     int c=0;
     while(c<candidates.size() & !found)
     {
-      if (hasRabbit(candidates.attempt.row, candidates.attempt.col)) {
-    	  showPath(); //shows solution. how is the path stored???
+      if (hasRabbit(candidates.get(c).attempt.row, candidates.get(c).attempt.col)) {
+    	  showPath(candidates, c);
     	  found = true;
       }
       else {
-    	  if (!hasWall(candidates.attempt.row - 1, candidates.attempt.col)) {
-    		candidates.add(new Candidate(new Attempt(candidates.attempt.row - 1, candidates.attempt.col),c));
+    	  if (!hasWall(candidates.get(c).attempt.row - 1, candidates.get(c).attempt.col)) {
+    		candidates.add(new Candidate(new Attempt(candidates.get(c).attempt.row - 1, candidates.get(c).attempt.col),c));
     		//add (A',c) to C
     	  }
-    	  if (!hasWall(candidates.attempt.row + 1, candidates.attempt.col)) {
-    		candidates.add(new Candidate(new Attempt(candidates.attempt.row + 1, candidates.attempt.col),c));
+    	  if (!hasWall(candidates.get(c).attempt.row + 1, candidates.get(c).attempt.col)) {
+    		candidates.add(new Candidate(new Attempt(candidates.get(c).attempt.row + 1, candidates.get(c).attempt.col),c));
     		//add (A',c) to C
     	  }  
-    	  if (!hasWall(candidates.attempt.row, candidates.attempt.col - 1)) {
-    		candidates.add(new Candidate(new Attempt(candidates.attempt.row, candidates.attempt.col - 1),c));
+    	  if (!hasWall(candidates.get(c).attempt.row, candidates.get(c).attempt.col - 1)) {
+    		candidates.add(new Candidate(new Attempt(candidates.get(c).attempt.row, candidates.get(c).attempt.col - 1),c));
     		//add (A',c) to C
     	  }
-    	  if (!hasWall(candidates.attempt.row, candidates.attempt.col + 1)) {
-    		candidates.add(new Candidate(new Attempt(candidates.attempt.row, candidates.attempt.col + 1),c));
+    	  if (!hasWall(candidates.get(c).attempt.row, candidates.get(c).attempt.col + 1)) {
+    		candidates.add(new Candidate(new Attempt(candidates.get(c).attempt.row, candidates.get(c).attempt.col + 1),c));
     		//add (A',c) to C
     	  }
-    	  c += 1;
+    	  c ++;
       }   	  
     }
   }
+
+private static void showPath(ArrayList<Candidate> candidates, int c) {
+	if (c == 0) {
+		System.out.println("Done");
+		return;
+	}
+	System.out.println(candidates.get(c).attempt.col);
+	System.out.print(", " +candidates.get(c).attempt.row);
+	showPath(candidates, c - 1);
+}
+
 }
